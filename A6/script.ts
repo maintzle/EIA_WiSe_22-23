955322263952652a0a7fc4ae3730ebbc48f71f08
maintzle <lm@momama.de>
2022-11-18 19:52:37 +0100

    A5 verbessert

---
+++ A6/script.ts
@@ -0,0 +1,10 @@
+const bevölkerungsdichtePolen2022 = 38179800 
+const peterine = "uggu"
+console.log("Einwohnerzahl Polen: " + bevölkerungsdichtePolen2022);
+const bevölkerungsdichtePolen1960 = 29640000
+const summe = bevölkerungsdichtePolen2022 - bevölkerungsdichtePolen1960
+console.log(summe);
+const prozentPolen = bevölkerungsdichtePolen1960 / 100 
+console.log(prozentPolen);
+const prozentergebnis = bevölkerungsdichtePolen2022 / prozentPolen
+console.log(prozentergebnis);
