8f13db9208cf5642c80435b16ad9a7b06fba4cd3
maintzle <lm@momama.de>
2022-11-23 20:02:08 +0100

    A6 verbessert

--- A6/script.ts
+++ A6/script.ts
@@ -1,34 +1,50 @@
+console.log("Polen");
 const bevölkerungsdichtePolen2022 = 38179800 
 const peterine = "uggu"
-console.log("Einwohnerzahl Polen: " + bevölkerungsdichtePolen2022);
+console.log("Einwohnerzahl: " + bevölkerungsdichtePolen2022);
 const bevölkerungsdichtePolen1960 = 29640000
-const summe = bevölkerungsdichtePolen2022 - bevölkerungsdichtePolen1960
-console.log(summe);
+const summePolen = bevölkerungsdichtePolen2022 - bevölkerungsdichtePolen1960
+console.log(summePolen);
 const prozentPolen = bevölkerungsdichtePolen1960 / 100 
 console.log(prozentPolen);
-const prozentergebnis = bevölkerungsdichtePolen2022 / prozentPolen
-console.log(prozentergebnis);
+const prozentergebnisPolen = bevölkerungsdichtePolen2022 / prozentPolen
+console.log(prozentergebnisPolen);
 
 
+console.log("Ukraine")
 const bevölkerungsdichteUkraine2022 = 43810000
 const kathrin = "uggu"
-console.log("Einwohnerzahl Polen: " + bevölkerungsdichteUkraine2022);
+console.log("Einwohnerzahl Ukraine: " + bevölkerungsdichteUkraine2022);
 const bevölkerungsdichteUkraine1960 = 42660000
-const summe = bevölkerungsdichteUkraine2022 - bevölkerungsdichteUkraine1960
-console.log(summe);
+const summeUkraine = bevölkerungsdichteUkraine2022 - bevölkerungsdichteUkraine1960
+console.log(summeUkraine);
 const prozentUkraine = bevölkerungsdichteUkraine1960 / 100 
 console.log(prozentUkraine);
-const prozentergebnis = bevölkerungsdichteUkraine2022 / prozentUkraine
-console.log(prozentergebnis);
+const prozentergebnisUkraine = bevölkerungsdichteUkraine2022 / prozentUkraine
+console.log(prozentergebnisUkraine);
 
 
+console.log("Niederlande")
 const bevölkerungsdichteNiederlande2022 = 17530000
 const ernst = "uggu"
-console.log("Einwohnerzahl Polen: " + bevölkerungsdichteNiederlande2022);
+console.log("Einwohnerzahl Niederlande: " + bevölkerungsdichteNiederlande2022);
 const bevölkerungsdichteNiederlande1960 = 11490000
-const summe = bevölkerungsdichteUkraine2022 - bevölkerungsdichteNiederlande1960
-console.log(summe);
+const summeNiederlande = bevölkerungsdichteNiederlande2022 - bevölkerungsdichteNiederlande1960
+console.log(summeNiederlande);
 const prozentNiederlande = bevölkerungsdichteNiederlande1960 / 100 
 console.log(prozentNiederlande);
-const prozentergebnis = bevölkerungsdichteNiederlande2022 / prozentNiederlande
-console.log(prozentergebnis);
+const prozentergebnisNiederlande = bevölkerungsdichteNiederlande2022 / prozentNiederlande
+console.log(prozentergebnisNiederlande);
+
+
+console.log("Luxemburg")
+const bevölkerungsdichteLuxemburg2022 = 639070
+const eduard = "uggu"
+console.log("Einwohnerzahl Luxemburg: " + bevölkerungsdichteLuxemburg2022);
+const bevölkerungsdichteLuxemburg1960 = 313970
+const summeLuxemburg = bevölkerungsdichteLuxemburg2022 - bevölkerungsdichteLuxemburg1960
+console.log(summeLuxemburg);
+const prozentLuxemburg = bevölkerungsdichteLuxemburg1960 / 100 
+console.log(prozentLuxemburg);
+const prozentergebnisLuxemburg = bevölkerungsdichteLuxemburg2022 / prozentLuxemburg
+console.log(prozentergebnisLuxemburg);
